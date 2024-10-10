ec87b55035d428c2b278ec432d17caf49bd56ff4
TalkingPotato90 <star901210@hanmail.net>
2024-10-10 16:15:25 +0900

    Feat: 당첨번호 중복 예외 처리 구현

--- src/main/java/lotto/view/Input.java
+++ src/main/java/lotto/view/Input.java
@@ -44,6 +44,10 @@ public class Input {
                 .forEach(number -> {
             throw new IllegalArgumentException("[ERROR] 당첨 번호는 1부터 45 사이의 숫자만 입력 가능합니다.");
         });
+
+        if(Arrays.stream(numbers).distinct().toArray().length != Limit.NUMBER_LENGTH.getValue()) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호는 중복이 없어야 합니다.");
+        }
     }
 
 }
