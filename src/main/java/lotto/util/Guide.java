a849e4a3958977dc42324bf029c0fbf877e7a44e
TalkingPotato90 <star901210@hanmail.net>
2024-10-10 15:03:39 +0900

    Feat: enum 데이터 추가

--- src/main/java/lotto/util/Guide.java
+++ src/main/java/lotto/util/Guide.java
@@ -1,4 +1,16 @@
 package lotto.util;
 
 public enum Guide {
+    PURCHASE("구입금액을 입력해 주세요.");
+
+
+    String message;
+
+    private Guide(String message){
+        this.message = message;
+    }
+
+    public String getMessage(){
+        return message;
+    }
 }
--- src/main/java/lotto/util/Limit.java
+++ src/main/java/lotto/util/Limit.java
@@ -1,4 +1,19 @@
 package lotto.util;
 
 public enum Limit {
+    RANDOM_MIN(1),
+    RANDOM_MAX(45),
+    NUMBER_LENGTH(6)
+    ;
+
+
+    int value;
+
+    private Limit(int value){
+        this.value = value;
+    }
+
+    public int getValue(){
+        return value;
+    }
 }
