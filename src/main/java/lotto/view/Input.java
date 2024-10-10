5ee512670eac4f0154678297008be652bd172b68
TalkingPotato90 <star901210@hanmail.net>
2024-10-10 15:23:35 +0900

    Feat: 금액 형식 검증 기능 구현

--- src/main/java/lotto/view/Input.java
+++ src/main/java/lotto/view/Input.java
@@ -6,4 +6,10 @@ public class Input {
     public String getInput() {
         return Console.readLine();
     }
+
+    public void validate(String input) {
+        if (!input.matches("^[0-9]*$")) {
+            throw new IllegalArgumentException("[ERROR] 금액은 숫자만 입력가능합니다.]");
+        }
+    }
 }
