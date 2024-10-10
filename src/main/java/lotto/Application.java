1fb77131f2d09949d6b73980f3073d0835fc0825
TalkingPotato90 <star901210@hanmail.net>
2024-10-10 15:08:49 +0900

    Feat: 사용자 입력을 받는 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,16 @@
 package lotto;
 
+import lotto.util.Guide;
+import lotto.view.Input;
+import lotto.view.Output;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Output output = new Output();
+        Input input = new Input();
+
+        output.printGuide(Guide.PURCHASE.getMessage());
+        input.getInput();
     }
 }
--- src/main/java/lotto/view/Input.java
+++ src/main/java/lotto/view/Input.java
@@ -1,4 +1,9 @@
 package lotto.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Input {
+    public String getInput() {
+        return Console.readLine();
+    }
 }
