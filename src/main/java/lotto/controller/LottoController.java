eea5263a7a0c140a0679f18266eeaa76a608b01e
TalkingPotato90 <star901210@hanmail.net>
2024-10-12 21:39:39 +0900

    Feat: 데이터 형 변환으로 삭제했던 입력 검증 테스트와 기능 추가

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,11 +1,14 @@
 package lotto.controller;
 
+import lotto.util.Guide;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class LottoController {
     public List<Integer> convertWinningNumber(String input){
+        validateWinningNumberInput(input);
         return Arrays.stream
                         (input
                                 .replaceAll(" ","")
@@ -15,7 +18,19 @@ public class LottoController {
     }
 
     public int convertDigit(String input){
+        validateDigitInput(input);
         return Integer.parseInt(input);
     }
-    
+
+    private void validateDigitInput(String input){
+        if (!input.matches(Guide.ONLY_DIGIT.getMessage())) {
+            throw new IllegalArgumentException("[ERROR] 숫자만 입력가능합니다.]");
+        }
+    }
+
+    private void validateWinningNumberInput(String input){
+        if (!input.matches(Guide.WINNING_NUMBER_FORMAT.getMessage())) {
+            throw new IllegalArgumentException("[ERROR] 당첨번호는 숫자와 쉼표만 입력 가능합니다.");
+        }
+    }
 }
---
+++ src/test/java/lotto/controller/LottoControllerTest.java
@@ -0,0 +1,35 @@
+package lotto.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class LottoControllerTest {
+    private LottoController controller;
+
+    @BeforeEach
+    void setUp() {
+        controller = new LottoController();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"일,이,삼,사,오,6", ".,2,3,4,5,6", "a,2,3,4,5,6"})
+    @DisplayName("입력받은 당첨 번호에 쉼표와 숫자 이외의 입력이 있으면 예외가 발생한다.")
+    void inputWinningNumberFormatError(String value){
+        assertThatThrownBy(()-> controller.convertWinningNumber(value))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR]");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {",","a","일"})
+    @DisplayName("보너스 번호 입력값이 숫자가 아니면 예외가 발생한다.")
+    void inputBonusNumberFormatError(String value){
+        assertThatThrownBy(()-> controller.convertDigit(value))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR]");
+    }
+}
